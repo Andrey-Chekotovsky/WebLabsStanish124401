1a0646e9bcc31258560b7e80b8d899c2f34fd9f1
vanyok <vanya.zaytsev.04@gmail.com>
2023-09-15 16:27:14 +0300

    add new writeln

--- ForGitLAb2/src/Main.java
+++ ForGitLAb2/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello мир!");
         System.out.println("Манера крутит мир!");
+        System.out.println("В дабл капе мои заны...");
     }
 }
